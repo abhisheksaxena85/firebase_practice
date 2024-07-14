a626cbd8d9e0e3117235ee62a2cd2048329bd74c
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2024-07-14 12:11:21 +0530

    Firebase PhoneNumber Auth with OTP

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_practice/firebase_options.dart';
+import 'package:firebase_practice/user_page.dart';
 import 'package:flutter/material.dart';
 import 'HomePage.dart';
 
-void main() {
+void main()async {
   WidgetsFlutterBinding.ensureInitialized(); //Making sure widget initialization
-  Firebase.initializeApp(
+  await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform
   ); //Initialising the Firebase App
+
+  //Getting firestore data
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,7 +26,28 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home:HomePage()
+      home: (FirebaseAuth.instance.currentUser) != null ? const UserPage(verificationID: "verificationID"): HomePage()
+    );
+  }
+}
+
+
+
+class Mypage extends StatefulWidget {
+  const Mypage({super.key});
+
+  @override
+  State<Mypage> createState() => _MypageState();
+}
+
+class _MypageState extends State<Mypage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('User Login Page after register'),
+        backgroundColor: Colors.blue,
+      ),
     );
   }
 }
